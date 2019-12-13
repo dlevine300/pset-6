cd56a7660ca1ed0e622abc73d7ee5e038a5e59b9
David Levine <55397183+dlevine300@users.noreply.github.com>
2019-12-13 11:26:06 -0500

    intial commit 2

--- js/app.js
+++ js/app.js
@@ -1,15 +1,16 @@
 window.onload = function() {
     document.getElementById("poppin").onclick = wassPoppin;
-    document.getElementById("1").onclick = list
-  }
-const wassPoppin = function() {
+}
+const wassPoppin= function() {
   let input = prompt('Hey elites, Nico is gay. Yes or No?:')
+do {
   if (input === null) {
     window.alert("u didn't say Yes so ur bad.")
   }
   if (input == 'no') {
-    window.alert('wrong')
+      window.alert('wrong, try again')
   } else if (input == 'yes') {
     window.alert('right')
   }
+} while (input === null || imput === 'no')
 };
