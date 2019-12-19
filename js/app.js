92e25e938dd914944bc6ab58135907b45cc4373e
David Levine <55397183+dlevine300@users.noreply.github.com>
2019-12-18 19:35:17 -0500

    finished list

--- js/app.js
+++ js/app.js
@@ -1,23 +1,3 @@
-window.onload = function() {
-    document.getElementById("poppin").onclick = wassPoppin;
-}
-
-const wassPoppin = function () {
-
-	do {
-		var input = (prompt("Hey elites, chris is gay. Yes or No? "))
-		if (input == null) {
-			alert("you didn't say yes. So try again")
-		}
-		else if (input == "no" || input == "No") {
-			alert('Wrong, try again.')
-		}
-		else if (input == "yes" || input == "Yes") {
-			alert("Right! good job.") break;
-		}
-	} while (input == "no" || input == "No" || input == null);
-};
-
 var myNodelist = document.getElementsByTagName("LI");
 var i;
 for (i = 0; i < myNodelist.length; i++) {
