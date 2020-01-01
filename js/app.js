7009951e34073bd462959461ecf79100ec9cda11
David Levine <55397183+dlevine300@users.noreply.github.com>
2019-12-31 23:31:39 -0500

    update

--- js/app.js
+++ js/app.js
@@ -1,14 +1,13 @@
-var myNodelist = document.getElementsByTagName("LI");
+var myToDoList = document.getElementsByTagName("LI");
 var i;
-for (i = 0; i < myNodelist.length; i++) {
+for (i = 0; i < myToDoList.length; i++) {
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
   span.className = "close";
   span.appendChild(txt);
-  myNodelist[i].appendChild(span);
+  myToDoList[i].appendChild(span);
 }
 
-// Click on a close button to hide the current list item
 var close = document.getElementsByClassName("close");
 var i;
 for (i = 0; i < close.length; i++) {
@@ -18,7 +17,6 @@ for (i = 0; i < close.length; i++) {
   }
 }
 
-// Add a "checked" symbol when clicking on a list item
 var list = document.querySelector('ul');
 list.addEventListener('click', function(ev) {
   if (ev.target.tagName === 'LI') {
@@ -26,13 +24,12 @@ list.addEventListener('click', function(ev) {
   }
 }, false);
 
-// Create a new list item when clicking on the "Add" button
 function newElement() {
   var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
+  var value = document.getElementById("myInput").value;
+  var t = document.createTextNode(value);
   li.appendChild(t);
-  if (inputValue === '') {
+  if (value === '') {
     alert("You must write something!");
   } else {
     document.getElementById("myUL").appendChild(li);
