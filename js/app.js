bc2a42470e87eb4df2f527496fe883ce55a88f9c
David Levine <55397183+dlevine300@users.noreply.github.com>
2020-01-06 17:32:43 -0500

    this is definately the most beautiful code ever

--- js/app.js
+++ js/app.js
@@ -1,51 +1,103 @@
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
+document.addEventListener("DOMContentLoaded", function () {
+     var add = document.getElementById("addTaskButton");
+     var removeFinished = document.getElementById("removeFinishedTasksButton");
+     var task = document.getElementById("taskInput");
+     var list = document.getElementById("taskList");
+     var body = document.querySelector("body");
+     var prior = document.getElementById("taskPriority");
 
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
-}
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('check');
-  }
-}, false);
+     //To do counter
+     var toDo = document.createElement("span");
+     body.insertBefore(toDo, list);
+     var allTasks = document.querySelectorAll("li");
+     allTasks.className = 'allTasks'
+     var counter = allTasks.length;
+     toDo.innerHTML = "Tasks to do: " + counter;
+     //Add task
+     add.addEventListener("click", function () {
+         if (task.value.length >= 5 &&
+         task.value.length <= 100 &&
+         prior.value > 0 &&
+         prior.value <= 10) {
 
-// Create a new list item when clicking on the "Add" button
-function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-    document.getElementById("myUL").appendChild(li);
-  }
-  document.getElementById("myInput").value = "";
+     //Add task to the list
 
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
+        var newTask = document.createElement("li");
+        newTask.dataset.priority = prior.value;
+        var all = document.querySelectorAll("li");
+        var index = all.length;
 
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
+        for (var i = 0; i < all.length; i++) {
+            if (parseInt(newTask.dataset.priority) > parseInt(all[i].dataset.priority)) {
+                index = i;
+                break;
+            }
+        }
+
+        list.insertBefore(newTask, all[index]);
+        var taskName = document.createElement("h1");
+        newTask.appendChild(taskName);
+        taskName.innerHTML = task.value;
+
+        //Add delete button
+        var delBtn = document.createElement("button");
+        delBtn.className ="delBtn"
+        newTask.appendChild(delBtn);
+        delBtn.innerHTML = "Delete";
+        delBtn.classList.add("delete");
+
+        //Add complete button
+        var complBtn = document.createElement("button");
+        complBtn.className = 'check'
+        newTask.appendChild(complBtn);
+        complBtn.innerHTML = "Complete";
+        complBtn.classList.add("complete");
+
+        counter++;
+        toDo.innerHTML = "Tasks to do: " + counter;
+
+        //Mark completed in red and adjust counter
+        const check = function () {
+            if (this.parentElement.style.color === "") {
+                this.parentElement.style.color = "blue";
+                this.parentElement.setAttribute("done", "yes");
+                counter--;
+                toDo.innerHTML = "Tasks to do: " + counter;
+            } else if (this.parentElement.style.color === "blue") {
+                this.parentElement.style.color = "";
+                this.parentElement.removeAttribute("done");
+                counter++;
+                toDo.innerHTML = "Tasks to do: " + counter;
+            }
+          }
+          check.className = 'addcheck'
+        complBtn.addEventListener("click", check);
+
+        //Delete selected item and adjust counter
+        delBtn.addEventListener("click", function () {
+            this.parentElement.parentNode.removeChild(this.parentElement);
+            counter--;
+            toDo.innerHTML = "Tasks to do: " + counter;
+        });
+
+        task.value = "";
+        prior.value = "";
+
+    } else {
+
+        event.preventDefault();
+        alert("Task should have from 5 to 100 characters. Priority should be an integer between 1 and 10");
     }
-  }
-}
+});
+
+//Remove completed items
+removeFinished.addEventListener("click", function () {
+    var tasks = document.querySelectorAll("li");
+    for (var i = 0; i < tasks.length; i++) {
+        if (tasks[i].hasAttribute("done")) {
+            tasks[i].parentNode.removeChild(tasks[i]);
+        }
+    }
+});
+
+});
